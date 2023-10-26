530280b47561f8f5c72b13d864d61627e03b9a37
topazape <topazape@gmail.com>
2023-10-26 15:49:13 +0900

    fix indent

--- plugin/md-preview.lua
+++ plugin/md-preview.lua
@@ -3,9 +3,9 @@ if loaded == 0 then
 	vim.g.loaded_md_preview = 1
 	-- Register commands
 	vim.cmd([[
-    command! MPToggle lua require('md-preview').toggle()
-    command! MPOpen lua require('md-preview').open()
-    command! MPClose lua require("md-preview").close()
-    command! MPRefresh lua require("md-preview").refresh()
+		command! MPToggle lua require('md-preview').toggle()
+ 		command! MPOpen lua require('md-preview').open()
+ 		command! MPClose lua require("md-preview").close()
+ 		command! MPRefresh lua require("md-preview").refresh()
   ]])
 end
